119e44f42b861dade501b7ba6c0a136836a889a2
mohammad-alzool <m.zool@outlook.com>
2021-02-12 22:49:57 +0200

    debugging

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@
 
 
 let finalScore=0 ;
-let userName = prompt ('welcome to my page what\`s you\`r name ?');
+let userName = prompt ('welcome to my page what`s you`r name ?');
 
-console.log('the name of the visitor user is ' + userName);
+console.log(`the name of the visitor user is ${userName}`);
 
 let myUsername = prompt ('my name is mohammad alzool ?', 'YES or NO' );
 
@@ -15,151 +15,127 @@ let Degree = prompt ('did i study MIS ?', 'YES or NO');
 let Exp = prompt ('i never worked before ?', 'YES or NO');
 
 
- function UserName(){
-switch  (myUsername.toUpperCase()) {
-    case 'YES' :
-        case 'Y':
-      console.log('my name is mohammad alzool ? = correct answer');
-      alert('my name is mohammad alzool ? = correct answer');
-      finalScore++
-      break ;
-    case  'NO':
-        case 'N':
-      console.log('my name is mohammad alzool ? = wrong answer');
-      alert('my name is mohammad alzool ? = wrong answer');
-      break;
-      default:
-        console.log('my name is mohammad alzool ? = wrong entry');
-        alert('my name is mohammad alzool ? = wrong entry');
-        break;
-          }}
-
- UserName()
-
-
- function UserAge(){
-switch  (myAge.toUpperCase()){
-    case 'YES':
-        case 'Y':
-      console.log('iam 30 years old ? = correct answer');
-      alert('iam 30 years old ? = correct answer');
-      finalScore++
-      break ;
-    case 'NO':
-        case 'N':
-      console.log('iam 30 years old ? = wrong answer');
-      alert('iam 30 years old ? = wrong answer');
-      break;
-      default:
-  console.log('iam 30 years old ? = wrong entry');
-  alert('iam 30 years old ? = wrong entry');
-  break;
-      }}
-    UserAge();
-    
-    
-
-function UserDgree() {
-switch  (Degree.toUpperCase()){
+function testUsername(){
+  switch (myUsername.toUpperCase()) {
+  case 'YES' :
+  case 'Y':
+    console.log('my name is mohammad alzool ? = correct answer');
+    alert('my name is mohammad alzool ? = correct answer');
+    finalScore++ ;
+    break ;
   case 'NO':
-      case 'N':
+  case 'N':
+    console.log('my name is mohammad alzool ? = wrong answer');
+    alert('my name is mohammad alzool ? = wrong answer');
+    break;
+  default:
+    console.log('my name is mohammad alzool ? = wrong entry');
+    alert('my name is mohammad alzool ? = wrong entry');
+    break; }}
+testUsername();
+
+function userAge(){
+  switch (myAge.toUpperCase()){
+  case 'YES':
+  case 'Y':
+    console.log('iam 30 years old ? = correct answer');
+    alert('iam 30 years old ? = correct answer');
+    finalScore++ ;
+    break ;
+  case 'NO':
+  case 'N':
+    console.log('iam 30 years old ? = wrong answer');
+    alert('iam 30 years old ? = wrong answer');
+    break;
+  default:
+    console.log('iam 30 years old ? = wrong entry');
+    alert('iam 30 years old ? = wrong entry');
+    break;
+  }}
+userAge();
+
+function userDegree() {
+  switch (Degree.toUpperCase()){
+  case 'NO':
+  case 'N':
     console.log('did i study MIS ? = correct answer');
     alert('did i study MIS ? = correct answer');
-    finalScore++
+    finalScore++ ;
     break ;
   case 'YES' :
-      case 'Y':
+  case 'Y':
     console.log('did i study MIS ? = wrong answer');
     alert('did i study MIS ? = wrong answer');
     break;
-    default:
-      console.log('did i study MIS ? = wrong entry');
-      alert('did i study MIS ? = wrong entry');
-      break;
-        }}
-UserDgree();
+  default:
+    console.log('did i study MIS ? = wrong entry');
+    alert('did i study MIS ? = wrong entry');
+    break;
+  }}
+userDegree();
 
 
-function Expo (){
-switch  (Exp.toUpperCase()){
-  case 'YES':
-      case 'Y':
-    console.log('i never worked before ? =correct answer');
-  alert('i never worked before ? =correct answer');
-  finalScore++
-    break ;
+function expo (){
+  switch (Exp.toUpperCase()){
   case 'NO' :
-      case 'N':
+  case 'N':
+    console.log('i never worked before ? =correct answer');
+    alert('i never worked before ? =correct answer');
+    finalScore++ ;
+    break ;
+  case 'YES':
+  case 'Y':
     console.log('i never worked before ? = wrong answer');
     alert('i never worked before ? = wrong answer');
     break;
-    default:
-      console.log('i never worked before ? = wrong entry');
-      alert('i never worked before ? = wrong entry');
-      break;
-        }}
-Expo();
+  default:
+    console.log('i never worked before ? = wrong entry');
+    alert('i never worked before ? = wrong entry');
+    break;
+  }}
+expo();
 
 
 
-let GuessesNumber = prompt ('guess a number', '1,2,3...999');
-let attempts
+let guessesNumber = prompt ('guess a number', '1,2,3...999');
+let attempts ;
 
-
-
-
-function test (){
-  if (GuessesNumber ==127){
-    alert('correct answer');
-
-    finalScore++ ;
-    
-  }
-  if (attempts ==1){
-    alert('You carried out your attempts , the correct answer was (127)');
-  }
-  else if (GuessesNumber <127){
-    alert('too low');
-    GuessesNumber = prompt ('guess a number again you still have '+(attempts-1) +' attempts left', '1,2,3...999');
-  }
-  else if (GuessesNumber >127){
-    alert('too high');
-    GuessesNumber = prompt ('guess a number again you still have '+(attempts-1) +' attempts left', '1,2,3...999');
-  
-  }
-  
-  else { 
-    alert('wrong entry , please enter numbers only  ')
-    GuessesNumber = prompt ('guess a number again you still have '+(attempts-1) +' attempts left', '1,2,3...999');
-  }
-  
- }
 for (attempts=4;attempts>0;--attempts){
- test()
+  if (guessesNumber ==='127'){
+    alert('correct answer');
+    finalScore++;
+    attempts=0;
+  }
+  else if (attempts ===1){
+    alert('You carried out your attempts , the correct answer was (127)');
+    attempts=0;
+  }
+  else if (guessesNumber <127){
+    alert('too low');
+    guessesNumber = prompt ('guess a number again you still have '+(attempts-1) +' attempts left', '1,2,3...999');
+  }
+  else if (guessesNumber >127){
+    alert('too high');
+    guessesNumber = prompt ('guess a number again you still have '+(attempts-1) +' attempts left', '1,2,3...999'); }
+  else {
+    alert('wrong entry , please enter numbers only  ');
+    guessesNumber = prompt ('guess a number again you still have '+(attempts-1) +' attempts left', '1,2,3...999');
+  }}
+const newLocal = ['Pirates of the Caribbean', 'Inception', 'Thor', 'Iron Man', 'X-Men', 'Limitless', 'Ex Machina', 'Avengers', 'The Hangover Part', 'Suicide Squad'];
+let moviesList=newLocal;
+let myFev = prompt ('name on of my favorite movies');
+let t;
+for (t=5;t>0;--t){
+  if (moviesList.includes(myFev)){
+    alert('true');
+    finalScore++ ;
+    break;
+  }else {
+    myFev = prompt('you have '+(t)+' attempts left' );
+  }
 }
-
-
-let moviesList=['Pirates of the Caribbean','Inception','Thor','Iron Man','X-Men','Limitless','Ex Machina','Avengers','The Hangover Part','Suicide Squad'];
-
-
-      console.log(moviesList);
-      let movieAttempts;
-      let t;
-      
-        let myFev = prompt ('name on of my favorite movies');
-      
-
-        for (let t=5;t>0;--t){
- if (moviesList.includes(myFev)) {
-   alert('true');
-   finalScore++
-   break;
- }else {
-   myFev = prompt('you have '+(t)+' attempts left' );
- }
-}
-if (t=1) {
-  alert('this is my favorite movies list  (Pirates of the Caribbean, Inception, Thor, Iron Man, X-Men, Limitless, Ex Machina, Avengers, The Hangover Part Suicide Squad)')  
+if (t===0) {
+  alert('this is my favorite movies list  (Pirates of the Caribbean, Inception, Thor, Iron Man, X-Men, Limitless, Ex Machina, Avengers, The Hangover Part Suicide Squad)');
 }
 alert('thank you '+ userName + ' for visiting my web page');
 alert('you`r score is '+finalScore+' of 7' );
